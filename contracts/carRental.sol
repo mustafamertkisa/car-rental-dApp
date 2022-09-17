0b46b914badfc2cd829ae81e763f4826ee3f7447
mustafamertkisa <m.mertkisa@gmail.com>
2022-09-17 14:30:55 +0300

    Added events for external functions

--- contracts/carRental.sol
+++ contracts/carRental.sol
@@ -27,6 +27,13 @@ contract carRental {
     Car[] public cars;
     Rent[] public rents;
 
+    /// ***** EVENTS *****
+
+    event AddNewCar(uint256 carId, string model, address owner);
+    event RentalRequest(uint256 carId, uint256 dayToRent, uint256 startDate);
+    event DeliveryCar(uint256 carId, uint256 rentId, uint256 price);
+    event ReturnCar(uint256 carId, uint256 rentId, uint256 dueDate);
+
     /// ****** EXTERNAL FUNCTIONS *****
 
     /// @notice car owners can add this function to their car to the system
@@ -41,6 +48,7 @@ contract carRental {
             msg.sender
         );
         cars.push(carPosting);
+        emit AddNewCar(currentId, _model, msg.sender);
     }
 
     /// @notice with this function the lessor can deposit the deposit and start the rental process
@@ -51,6 +59,9 @@ contract carRental {
         uint256 calculatedDeposit = calculateDeposit(_carId);
         require(cars[_carId].isAvailable, "Car not available");
         require(msg.value >= calculatedDeposit, "Insufficient deposit");
+        (bool depositSent, bytes memory data) = address(this).call{
+            value: msg.value
+        }("");
         cars[_carId].isAvailable = false;
         uint256 currentId = rents.length;
         Rent memory rentDetail = Rent(
@@ -64,9 +75,7 @@ contract carRental {
             false
         );
         rents.push(rentDetail);
-        (bool depositSent, bytes memory data) = address(this).call{
-            value: msg.value
-        }("");
+        emit RentalRequest(_carId, _dayToRent, block.timestamp);
     }
 
     /// @notice when the car is delivered, the car owner is paid, the function is only available to the hirer
@@ -82,6 +91,7 @@ contract carRental {
         (bool sentPrice, bytes memory data) = cars[_carId].owner.call{
             value: msg.value
         }("");
+        emit DeliveryCar(_carId, _rentId, calculatedPrice);
     }
 
     /// @notice used when refunded in between, the deposit is refunded to the renter if the time has not expired, the function can only be used by the owner of the car
@@ -89,8 +99,7 @@ contract carRental {
         require(msg.sender == cars[_carId].owner, "Invalid owner");
         require(_carId == rents[_rentId].carId, "Dont match car id");
         require(!rents[_rentId].isComplete, "Process completed");
-        cars[_carId].isAvailable = true;
-        rents[_rentId].isComplete = true;
+
         uint256 dueDate = rents[_rentId].rentalStartTime +
             ((rents[_rentId].dayToRent * 24) * 3600);
 
@@ -103,6 +112,9 @@ contract carRental {
                 value: rents[_rentId].deposit
             }("");
         }
+        cars[_carId].isAvailable = true;
+        rents[_rentId].isComplete = true;
+        emit ReturnCar(_carId, _rentId, dueDate);
     }
 
     /// ****** PUBLIC FUNCTIONS *****
